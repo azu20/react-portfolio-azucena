95b8cb448447ccffe222fc96e40e59a73d5674c1
Azucena Herrera <azucenaphx@gmail.com>
2020-07-24 01:29:25 -0700

    updated nabar

--- suzy-portfolio/src/components/Navbar/index.js
+++ suzy-portfolio/src/components/Navbar/index.js
@@ -5,20 +5,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function Navbar() {
     return (
     <nav class="navbar navbar-expand-lg">
-        <a class="navbar-brand" href="./Pages/AboutMe">Azucena Gonzalez</a>
+        <a class="navbar-brand" href="../Pages/AboutMe">Azucena Gonzalez</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
             <ul class="navbar-nav">
                 <li class="nav-item active">
-                    <a class="nav-link" href="./Pages/AboutMe">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="../Pages/AboutMe">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="./Pages/Portfolio">Portfolio</a>
+                    <a class="nav-link" href="../Pages/Portfolio">Portfolio</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="./Pages/Contact">Contact</a>
+                    <a class="nav-link" href="../Pages/Contact">Contact</a>
                 </li>
             </ul>
         </div>
